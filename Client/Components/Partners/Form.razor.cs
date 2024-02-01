4c57148e61406bd4cae7c9240c2f761d45d107cc
Alexey Fedorov <fedorov.alexey.02@gmail.com>
2024-02-01 13:16:23 +0300

    Продолжил дорабатывать раздел партнеров. Использовал общие контролы на форме ввода партнера, вынес логику формы в отдельный файл.

---
+++ Client/Components/Partners/Form.razor.cs
@@ -0,0 +1,77 @@
+﻿using Client.Services;
+using DataBase.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Client.Components.Partners;
+
+public partial class Form: ComponentBase
+{
+    [Inject]
+    private IPartnerTypesService PartnerTypesService { get; set; }
+    [Inject]
+    private IDirectionsService DirectionsService { get; set; }
+
+    [Parameter]
+    public Partner Partner { get; set; } = null!;
+    [Parameter]
+    public EventCallback OnSubmit { get; set; }
+
+    private IEnumerable<PartnerType>? partnerTypes;
+    private List<Direction>? directions;
+    private string PartnerTypeId => Partner.PartnerTypeId != 0 ? Partner.PartnerTypeId.ToString() : "Выберите опцию";
+
+
+    protected async override Task OnParametersSetAsync()
+    {
+        SetDirectionsToPartner();
+        var directionsTask = LoadDirections();
+        var partnerTypesTask = LoadPartnerTypes();
+        await directionsTask;
+        await partnerTypesTask;
+    }
+
+    private void SetDirectionsToPartner()
+    {
+        if (Partner.Directions is not null) return;
+        Partner.Directions = [];
+    }
+
+    private async Task LoadDirections()
+    {
+        try
+        {
+            directions = await DirectionsService.GetDirections();
+        }
+        catch (Exception ex)
+        {
+            //TODO Добавить обработку исключений
+            Console.WriteLine($"Исключение при загрузке списка направлений: {ex}");
+        }
+    }
+
+    private async Task LoadPartnerTypes()
+    {
+        Console.WriteLine("Загрузка списка типов партнеров");
+        try
+        {
+            partnerTypes = await PartnerTypesService.GetPartnerTypesAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Исключение при загрузке списка типов партнеров: {ex}");
+        }
+    }
+
+    private void OnPartnerTypeSelected(string id)
+    {
+        Console.WriteLine($"Выбран тип партнера {id}");
+        var partnerTypeId = int.Parse(id);
+        Partner.PartnerTypeId = partnerTypeId;
+    }
+
+    private async Task OnValidSubmit()
+    {
+        Console.WriteLine(Partner);
+        await OnSubmit.InvokeAsync();
+    }
+}
---
+++ Client/Components/Partners/List.razor.cs
@@ -0,0 +1,36 @@
+﻿using DataBase.Models;
+using Microsoft.AspNetCore.Components;
+using SharedModel;
+
+namespace Client.Components.Partners;
+
+public partial class List
+{
+    IEnumerable<PartnerForList> Partners { get; set; } = new List<PartnerForList>();
+
+    private string message = "Загрузка...";
+
+    async Task LoadPartners()
+    {
+        Console.WriteLine("Начата загрузка списка партнеров");
+        try
+        {
+            Partners = await PartnersService.GetPartners();
+        }
+        catch (Exception ex)
+        {
+            message = $"Поймали исключение на списке партнеров: {ex}";
+        }
+    }
+
+    void OpenPartnerDetails(Partner partner)
+    {
+        NavigationManager.NavigateTo("/person/" + partner.Id.ToString());
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Console.WriteLine("On init");
+        await LoadPartners();
+    }
+}
