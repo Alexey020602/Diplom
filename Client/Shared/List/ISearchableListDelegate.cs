512988b65315d1499887412786b7e1158f14af2b
Alexey Fedorov <fedorov.alexey.02@gmail.com>
2024-04-07 19:40:19 +0300

    Разделил Shared компоненты на разные папки

---
+++ Client/Shared/List/ISearchableListDelegate.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Client.Shared.List;
+
+public interface ISearchableListDelegate<TItem>
+{
+    public Task<List<TItem>> LoadItems();
+    public string Row(TItem item);
+    public RenderFragment Filters { get; }
+    public string PathForCreate { get; }
+}
