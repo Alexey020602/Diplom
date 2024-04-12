02949fe9cf5211e9e9f565b3569975d02c38d85e
Alexey Fedorov <fedorov.alexey.02@gmail.com>
2024-04-12 19:59:22 +0300

    Сделал базовый компонент для формы

--- Client/Components/Partners/Form.razor.cs
+++ Client/Components/Partners/Form.razor.cs
@@ -4,60 +4,65 @@ using Microsoft.AspNetCore.Components;
 
 namespace Client.Components.Partners;
 
-public partial class Form: ComponentBase
-{
-    [Inject]
-    private IPartnerTypesService PartnerTypesService { get; set; } = default!;
-    [Inject]
-    private IDirectionsService DirectionsService { get; set; } = default!;
+//public partial class Form: ComponentBase
+//{
+//    [Inject] private NavigationManager NavigationManager { get; set; } = default!;
+//    [Inject] private IPartnerTypesService PartnerTypesService { get; set; } = default!;
+//    [Inject] private IDirectionsService DirectionsService { get; set; } = default!;
 
-    [Parameter]
-    public Partner Partner { get; set; } = null!;
-    [Parameter]
-    public EventCallback OnSubmit { get; set; }
+//    [Parameter]
+//    public Partner Partner { get; set; } = null!;
+//    [Parameter]
+//    public EventCallback OnSubmit { get; set; }
+//    [Parameter] public string PreviousScreenUri { get; set; } = string.Empty;
 
-    private IEnumerable<PartnerType>? partnerTypes;
-    private List<Direction>? directions;
-    protected async override Task OnParametersSetAsync()
-    {
-        var directionsTask = LoadDirections();
-        var partnerTypesTask = LoadPartnerTypes();
-        await directionsTask;
-        await partnerTypesTask;
-    }
+//    private IEnumerable<PartnerType>? partnerTypes;
+//    private List<Direction>? directions;
+//    protected async override Task OnParametersSetAsync()
+//    {
+//        var directionsTask = LoadDirections();
+//        var partnerTypesTask = LoadPartnerTypes();
+//        await directionsTask;
+//        await partnerTypesTask;
+//    }
 
-    private async Task LoadDirections()
-    {
-        try
-        {
-            var directions = await DirectionsService.ReadAll();
-            Console.WriteLine(directions);
-            this.directions = directions
-                .Where(direction => !Partner.Directions.Contains(direction))
-                .ToList(); 
-        }
-        catch (Exception ex)
-        {
-            //TODO Добавить обработку исключений
-            Console.WriteLine($"Исключение при загрузке списка направлений: {ex}");
-        }
-    }
-    private async Task LoadPartnerTypes()
-    {
-        Console.WriteLine("Загрузка списка типов партнеров");
-        try
-        {
-            partnerTypes = await PartnerTypesService.ReadAll();
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Исключение при загрузке списка типов партнеров: {ex}");
-        }
-    }
+//    private async Task LoadDirections()
+//    {
+//        try
+//        {
+//            var directions = await DirectionsService.ReadAll();
+//            Console.WriteLine(directions);
+//            this.directions = directions
+//                .Where(direction => !Partner.Directions.Contains(direction))
+//                .ToList(); 
+//        }
+//        catch (Exception ex)
+//        {
+//            //TODO Добавить обработку исключений
+//            Console.WriteLine($"Исключение при загрузке списка направлений: {ex}");
+//        }
+//    }
+//    private async Task LoadPartnerTypes()
+//    {
+//        Console.WriteLine("Загрузка списка типов партнеров");
+//        try
+//        {
+//            partnerTypes = await PartnerTypesService.ReadAll();
+//        }
+//        catch (Exception ex)
+//        {
+//            Console.WriteLine($"Исключение при загрузке списка типов партнеров: {ex}");
+//        }
+//    }
 
-    private async Task OnValidSubmit()
-    {
-        Console.WriteLine(Partner);
-        await OnSubmit.InvokeAsync();
-    }
-}
+//    private async Task OnValidSubmit()
+//    {
+//        Console.WriteLine(Partner);
+//        await OnSubmit.InvokeAsync();
+//        GoToPreviousScreen();
+//    }
+//    private void GoToPreviousScreen()
+//    {
+//        NavigationManager.NavigateTo(PreviousScreenUri);
+//    }
+//}
---
+++ Client/Shared/Form/BaseForm.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+namespace Client.Shared.Form;
+
+public abstract partial class BaseForm<TItem> where TItem : class
+{
+    [Inject] private IJSRuntime JSRuntime { get; set; } = null!;
+    [Parameter] public TItem Item { get; set; } = null!;
+    [Parameter] public EventCallback OnSubmit { get; set; }
+    protected async Task OnValueSubmit()
+    {
+        await OnSubmit.InvokeAsync();
+        await JSRuntime.InvokeVoidAsync("history.back");
+    }
+    protected abstract RenderFragment Content { get; }
+}
