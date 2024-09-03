b56ada9849e4eaaa0301ac7647b3794d80c21116
Alexey Fedorov <fedorov.alexey.02@gmail.com>
2024-09-03 22:11:44 +0300

    Начал делать сраницу регистрации

--- Client/Services/Authorization/IAuthStateChangeNotifier.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components.Authorization;
-
-namespace Client.Services.Authorization;
-
-public interface IAuthStateChangeNotifier
-{
-    public void NotifyUserAuthentication(Dto.Authorization authorization);
-    public void NotifyUserLogout();
-}
