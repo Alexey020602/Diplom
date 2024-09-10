1acca75977878cc3bfe81f2552ce9b169eb276e9
Alexey Fedorov <fedorov.alexey.02@gmail.com>
2024-09-10 18:02:30 +0300

    Переделал страницу регистрации

--- Model/Identity/RegistrationRequest.cs
+++ Model/Identity/RegistrationRequest.cs
@@ -4,9 +4,12 @@ namespace Model.Identity;
 
 public class RegistrationRequest
 {
-    [Required] public string? Login { get; set; }
-    [Required] public string? Password { get; set; }
+    [Required(ErrorMessage = "Необходимо ввести логин")] 
+    public string? Login { get; set; }
+    [Required(ErrorMessage = "Необходимо ввести пароль")]
+    [MinLength(6, ErrorMessage = "Пароль должен быть длинной не менее 6 сиволов")]
+    public string? Password { get; set; }
 
     [MinLength(1, ErrorMessage = "Необходимо выбрать хотя бы одну роль")]
-    public List<Role> Roles { get; set; }
+    public IEnumerable<Role> Roles { get; set; } = [];
 }
--- Model/Identity/RegistrationResponse.cs
+++ Model/Identity/RegistrationResponse.cs
@@ -5,5 +5,5 @@ namespace Model.Identity;
 public class RegistrationResponse
 {
     [Required] public string? Login { get; set; }
-    public List<Role> Roles { get; set; }
+    public IEnumerable<Role> Roles { get; set; } = default!;
 }
