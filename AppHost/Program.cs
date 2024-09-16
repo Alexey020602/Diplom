c88485bd5a614569c2df9fa6b5267e65748c4d8f
Alexey Fedorov <fedorov.alexey.02@gmail.com>
2024-09-16 18:20:49 +0300

    Добавил базу данных в контейнер и настроил ее использование в проекте Diploma.csproj

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,3 +1,12 @@
+using Microsoft.Extensions.Hosting;
 var builder = DistributedApplication.CreateBuilder(args);
-builder.AddProject<Projects.Diploma>("diploma");
+var username = builder.AddParameter("username", true);
+var password = builder.AddParameter("password", true);
+var postgres = builder
+    .AddPostgres("postgres", password: password)
+    // .PublishAsConnectionString()
+    .WithPgAdmin()
+    .AddDatabase("DiplomaDb");
+builder.AddProject<Projects.Diploma>("diploma")
+    .WithReference(postgres);
 builder.Build().Run();
